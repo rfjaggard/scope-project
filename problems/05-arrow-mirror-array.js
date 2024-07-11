818d67fd521acbf7eaf302c4de40bcd5c882b0f7
tydoesfx <tydoesfx@gmail.com>
2024-07-11 18:57:47 -0400

    6th done

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -11,6 +11,15 @@ arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b
 ***********************************************************************/
 
 // Your code here 
+const arrowMirrorArray = (array) => {
+ /*   const arr1 = [array];
+    arr1.reverse(); */
+  return array.concat(array.slice().reverse())
+}
+
+
+arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
