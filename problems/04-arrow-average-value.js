2284323a22bf8fcec2280e18b609184f48bfcf38
tydoesfx <tydoesfx@gmail.com>
2024-07-11 18:32:51 -0400

    5th Done

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -11,7 +11,15 @@ arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
 // Your code here 
+const arrowAvgValue = (array) => 
+  {const sum = array.reduce((acc, curr) => (acc + curr))
+    return sum / array.length}
 
+
+
+arrowAvgValue([10, 20]); // => 15
+arrowAvgValue([2, 3, 7]); // => 4
+arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
